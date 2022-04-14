73ab40ad9a6581b212a764f839dd8659a0804018
Rajni2002 <rajnikantdash12@gmail.com>
2022-04-15 00:04:19 +0530

    Level - 0 [status: compeleted]

--- app.js
+++ app.js
@@ -9,31 +9,56 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 // Schema and model for Monthly Budget
 const BudgetSchema = new mongoose.Schema({
   income: Number,
   year: Number,
-  month: Number,//1 based indexing
+  month: Number, //1 based indexing
   expenses: [{ category: String, amount: String, colorCode: String }],
 });
 
 const Budgets = mongoose.model("Budget", BudgetSchema);
-  
 
-app.get("/", function (req, res) {
-  res.render("monthlyBudget");
+app.get("/budget", function (req, res) {
+  let currdate = new Date();
+  Budgets.find(
+    { year: currdate.getFullYear(), month: currdate.getMonth() },
+    function (err, result) {
+      if (err) console.log(err);
+      else {
+        if(result.length == 0){
+          res.render("enter");
+        }else{
+          // res.send("Rendered current month Budget", result);
+          res.render("currBudget", {
+            expenseList: result[0].expenses
+          });
+        }
+      }
+    }
+  );
 });
 
+app.get("/fillBudget", function(req,res){
+  res.render("editMonthlyBudget");
+})
+
 app.post("/jsondata", function (req, res) {
-    console.log(req.body);
-    const currBudget = new Budgets({
-      income: Number(req.body.income),
-      year: req.body.year,
-      month: req.body.month,
-      expenses: req.body.expenseList
-    });
-    currBudget.save();
+  let currdate = new Date();
+  console.log(req.body);
+  Budgets.deleteOne({year: currdate.getFullYear(), month: currdate.getMonth()}).then(function(){
+    console.log("Deleted");
+  }).catch(function(err){
+    console.log("error", err);
+  });
+  const currBudget = new Budgets({
+    income: Number(req.body.income),
+    year: req.body.year,
+    month: req.body.month,
+    expenses: req.body.expenseList,
+  });
+  currBudget.save();
+  res.redirect("/budget");
 });
 
 app.listen(3000, function () {
---
+++ public/js/index.js
@@ -0,0 +1,113 @@
+const monthArray = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let currentDate = new Date();
+let currMonth = currentDate.getMonth();
+document.querySelector(".currMonth").innerText = monthArray[currMonth];
+
+function trigger() {
+  let del = document.querySelectorAll(".delete");
+  for (let i = 0; i < del.length; i++) {
+    del[i].addEventListener("click", function () {
+      let parent = this.parentElement;
+      parent.remove();
+    });
+  }
+}
+
+function newElement() {
+  // Fetching the values
+  let Category = document.getElementById("category").value;
+  let Amount = document.getElementById("amount").value;
+  let colorSelect = document.getElementById("color-select").value;
+  // creating the list item and appending it
+
+  if (Category === "" || Amount === "") {
+    alert("You must write something!");
+  } else {
+    let expenseDiv = document.createElement("div");
+    expenseDiv.classList += "expense";
+
+    let divCat = document.createElement("div");
+    let divAmt = document.createElement("div");
+    let divCol = document.createElement("div");
+    let divDel = document.createElement("div");
+
+    divCat.classList += "category";
+    divAmt.classList += "amount";
+    divCol.classList += "color-select";
+    divDel.classList += "delete";
+
+    let pCat = document.createElement("p");
+    let pAmt = document.createElement("p");
+    let pDel = document.createElement("p");
+
+    pCat.innerText = Category;
+    divCat.appendChild(pCat);
+    pAmt.innerText = Amount;
+    divAmt.appendChild(pAmt);
+    pDel.innerText = "X";
+    divDel.appendChild(pDel);
+
+    let divColTag = document.createElement("div");
+    divColTag.style.width = "20px";
+    divColTag.style.height = "20px";
+    divColTag.style.backgroundColor = colorSelect;
+    divColTag.style.borderRadius = "50%";
+    divColTag.style.position = "relative";
+    divColTag.style.top = "14px";
+    divCol.appendChild(divColTag);
+
+    expenseDiv.appendChild(divCat);
+    expenseDiv.appendChild(divAmt);
+    expenseDiv.appendChild(divCol);
+    expenseDiv.appendChild(divDel);
+
+    document.querySelector(".expenses").appendChild(expenseDiv);
+  }
+  // Resetting the input values
+  document.getElementById("category").value = "";
+  document.getElementById("amount").value = "";
+  document.getElementById("color-select").value = "#000000";
+  trigger();
+}
+function sendData() {
+  let expenseList = [];
+  let income = document.getElementById("income").value;
+  let category = document.querySelectorAll(".category p");
+  let amount = document.querySelectorAll(".amount p");
+  let colorCode = document.querySelectorAll(".color-select div");
+  for (let i = 0; i < category.length; i++) {
+    let object = {
+      category: category[i].innerText,
+      amount: amount[i].innerText,
+      colorCode: colorCode[i].style.backgroundColor,
+    };
+    console.log(object);
+    expenseList.push(object);
+  }
+  fetch("/jsondata", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      year: currentDate.getFullYear(),
+      month: currMonth,
+      expenseList: expenseList,
+      income: income,
+    }),
+  });
+  window.open("/budget","_self");
+}
